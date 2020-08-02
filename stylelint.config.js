328bc40891ea7f58715d71acaff8b5f6206e44a0
kanako <4190346+mkanako@users.noreply.github.com>
2020-08-02 21:02:41 +0800

    up

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,9 @@ module.exports = {
   rules: {
     'no-descending-specificity': null,
     'no-empty-source': null,
+    'at-rule-no-unknown': [true, {
+      ignoreAtRules: ['tailwind'],
+    }],
   },
   ignoreFiles: [
     './src/views/Test.dev.vue',
