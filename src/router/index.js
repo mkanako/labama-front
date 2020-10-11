2269e93de047eec6e934b8771d5bfc99f4038464
kanako <4190346+mkanako@users.noreply.github.com>
2020-10-12 02:19:59 +0800

    up

--- src/router/index.js
+++ src/router/index.js
@@ -9,19 +9,6 @@ import { GenerateMenus } from '@/layout/LayoutSiderMenu'
 
 Vue.use(Router)
 
-function addExtraRoutes (routes) {
-  if (process.env.NODE_ENV === 'development') {
-    const { PageTest } = require('./defaultRoutes')
-    routes.push(PageTest)
-  }
-  routes.push(Page404)
-  routes.forEach(item => {
-    if (!item.component) {
-      item.component = Layout
-    }
-  })
-}
-
 const router = new Router({
   base: process.env.BASE_URL,
   scrollBehavior: () => ({ x: 0, y: 0 }),
@@ -35,11 +22,49 @@ const router = new Router({
   },
 })
 
-export function GenerateRoutes (routeList) {
-  const routes = dynamicRoutes.slice(0)
+function allRoutes () {
+  const isUrl = obj => {
+    if (obj.meta && obj.meta.type && obj.meta.type === 'url') {
+      return true
+    }
+    return /^https?:\/\//.test(obj.path)
+  }
+  function checkIsUrl (item) {
+    item.isUrl = isUrl(item)
+    if (item.children) {
+      item.children.forEach(children => {
+        checkIsUrl(children)
+      })
+    }
+  }
 
-  addExtraRoutes(routes)
-  router.addRoutes(routes)
+  if (process.env.NODE_ENV === 'development') {
+    const { PageTest } = require('./defaultRoutes')
+    dynamicRoutes.push(PageTest)
+  }
+  dynamicRoutes.push(Page404)
+  dynamicRoutes.forEach(item => {
+    checkIsUrl(item)
+    if (!item.isUrl && !item.component) {
+      item.component = Layout
+    }
+  })
+  return dynamicRoutes
+}
+
+export function GenerateRoutes (allowedRouteList) {
+  const routes = allRoutes()
+  if (!allowedRouteList.includes('*')) {
+  }
+  router.addRoutes(routes.filter(item => !item.isUrl).map(item => {
+    if (item.children) {
+      const children = item.children.filter(item => !item.isUrl)
+      if (children.length !== item.children.length) {
+        return Object.assign({}, item, { children })
+      }
+    }
+    return item
+  }))
   GenerateMenus(routes)
 }
 
