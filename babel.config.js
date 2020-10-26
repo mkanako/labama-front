4c023ec01641af645aafc85614e2d2f8a9e11db9
kanako <4190346+mkanako@users.noreply.github.com>
2020-10-27 03:27:43 +0800

    up

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
+  env: {
+    development: {
+      plugins: ['dynamic-import-node'],
+    },
+  },
   plugins: [
     [
       'import',
