69aa20d5ebd441654669c9b4dfef32df157fa4b7
kanako <4190346+mkanako@users.noreply.github.com>
2020-10-27 03:28:21 +0800

    up

--- scripts/ram-cache.sh
+++ scripts/ram-cache.sh
@@ -8,6 +8,9 @@ src='node_modules/.cache'
 
 if [[ -e $ramdisk ]]; then
   if [[ $(df -kP) =~ "$(pwd)/$src" ]]; then
+    if [[ $1 == 'mount' ]]; then
+      exit
+    fi
     echo "'$(pwd)/$src' is mounted."
     read -p 'Do you want to umount it? (y/n)' answer
     if [[ $answer == 'y' ]]; then
@@ -24,9 +27,11 @@ if [[ -e $ramdisk ]]; then
       mkdir $src
     fi
     bindfs $dist $src && echo 'ok'
+    exit
   else
     echo 'env: `npm_package_name` not exist'
   fi
 else
   echo "$ramdisk not exist"
 fi
+exit 1
